1aae792aa7ea6344e604d84fe88b51f647e15c99
Pranav-Agarwal <pranav1998ag@gmail.com>
2020-11-17 00:54:03 +0530

    assignment 5 submission

--- src/main/java/edu/neu/coe/info6205/sort/par/bottomUpPar.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/bottomUpPar.java
@@ -22,27 +22,21 @@ class bottomUpPar{
 	private void merge(int from,int mid,int to) {
 		if(mid>=to) return;
 		if(arr[mid]>=arr[mid-1]) return;
-		//System.out.println(Arrays.toString(Arrays.copyOfRange(arr, from, to+1)));
 		int[] temp = new int[to-from+1];
-		//System.out.print("merging from "+from+" to "+to+" on Thread "+Thread.currentThread().getName()+" with l "+from+" m "+mid+" r "+to+"---");
 		for(int i=from;i<=to;i++) {
-			//System.out.print(" "+i);
 			temp[i-from] = arr[i];
 		}
-		//System.out.println("");
 		int l = 0;
 		int m = mid-from;
 		int r = m;
 		int counter=from;
 		while(l<m || r<temp.length) {
-			//System.out.println("----- merging  "+l+" to "+r+" on Thread "+Thread.currentThread().getName());
 			if(l>=m) arr[counter++] = temp[r++];
 			else if(r>=temp.length) arr[counter++] = temp[l++];
 			else if(temp[l]<=temp[r]) arr[counter++] = temp[l++];
 			else arr[counter++] = temp[r++];
 		}
-		//System.out.println("merged from "+from+" to "+to+" on Thread "+Thread.currentThread().getName()+"--");
-		//System.out.println(Arrays.toString(Arrays.copyOfRange(arr, from, to+1)));
+		//System.out.println("merged from "+from+" to "+to+" on Thread "+Thread.currentThread().getName());
 	}
 	
 	private void sort(int from,int to) {
@@ -71,10 +65,8 @@ class bottomUpPar{
 		//MERGING SORTED SEGMENTS
 		int size = cutoff;
 		while(size<=arr.length) {
-			//System.out.println(size);
 			int merge_segment_start = 0;
 			while(merge_segment_start<arr.length) {
-				//System.out.println(merge_segment_start);
 				final int start = merge_segment_start;
 				final int mid = Math.min(start+size,arr.length-1);
 				final int end = Math.min(start+(size*2)-1,arr.length-1);
--- src/main/java/edu/neu/coe/info6205/sort/par/bottomUpSortDriver.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/bottomUpSortDriver.java
@@ -1,31 +1,46 @@
 package edu.neu.coe.info6205.sort.par;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 public class bottomUpSortDriver {
 	
-	private static boolean isSorted(int[] arr ) {
-		for(int i=1;i<arr.length;i++) {
-			if(arr[i]<arr[i-1]) return false;
-		}
-		return true;
-	}
+	static final int THREAD_COUNT = 8;
+	static final int MIN_ARRAY_SIZE = 1048576;
+	static final int MAX_ARRAY_SIZE = 16777216;
+	static final int MIN_CUTOFF = 16384;
+	static final int MAX_CUTOFF = 134217728;
 
 	public static void main(String[] args) {
+		
 		Random random = new Random();
-		int[] arr = new int[10000000];
-		for(int i=0;i<arr.length;i++) arr[i] = random.nextInt(10000);
-		bottomUpPar sorter = new bottomUpPar(10000,arr,2);
-		long startTime = System.currentTimeMillis();
+		
 		try {
-			sorter.sort();
+			FileWriter writer = new FileWriter("results/parsort/data3.csv",false);
+			writer.write("array_size,thread_count,cutoff,time_taken\n");
+			
+			for(int size=MIN_ARRAY_SIZE;size<=MAX_ARRAY_SIZE;size*=2) {
+				for(int cutoff=MIN_CUTOFF;cutoff<=MAX_CUTOFF;cutoff*=2) {
+					if(cutoff>size) continue;
+					System.out.println("sorting array of size "+size+" with cutoff "+cutoff);
+					int[] arr = new int[size];
+					for(int i=0;i<arr.length;i++) arr[i] = random.nextInt(100000);
+					bottomUpPar sorter = new bottomUpPar(cutoff,arr,THREAD_COUNT);
+					long startTime = System.currentTimeMillis();
+					sorter.sort();
+					long elapsedTime = System.currentTimeMillis()-startTime;
+					
+					writer.write(size+","+THREAD_COUNT+","+cutoff+","+elapsedTime+"\n");
+				}
+			}
+			System.out.println("benchmark completed");
+			writer.close();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		long endTime = System.currentTimeMillis();
-		//System.out.println(Arrays.toString(arr));
-		System.out.println("Time taken: "+(endTime-startTime)+ "ms, Sorted: "+isSorted(arr));
 	}
 
 }
